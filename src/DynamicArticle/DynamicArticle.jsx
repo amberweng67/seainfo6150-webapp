b335fad36af39e0aefc67c0d7d141cce667020a9
amberweng67 <amberweng67@gmail.com>
2021-03-06 17:36:34 -0800

    [React App Assignment]

--- src/DynamicArticle/DynamicArticle.jsx
+++ src/DynamicArticle/DynamicArticle.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import HTMLText from "../HTMLText/HTMLText";
+import {Link} from "react-router-dom";
+
 
 const DynamicArticle = ({article}) => {
   return (
     <article>
       <header>
+        <Link to="/articlelist">Back</Link>
         <h1>{article.title}</h1>
         <address>{article.author} (
           <a href={"mailto:" + article.authorEmail}>{article.authorEmail}</a>)
@@ -12,7 +15,7 @@ const DynamicArticle = ({article}) => {
         </address>
         <time dateTime={article.timeStamp}>{article.displayDate}</time>
         <br />
-        <img src={article.image._url} alt="News Picture" width="1100" height="619" />
+        <img src={article.image._url} alt="News" width="1100" height="619" />
       </header> 
     
       <HTMLText text={article.text} />
